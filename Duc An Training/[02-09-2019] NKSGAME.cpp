532e2de24216e42b23a378d1f3e47464a165ebd6
btdat2506 <btdat2506@users.noreply.github.com>
2019-09-02 23:46:27 +0700

    Update [02-09-2019] NKSGAME.cpp

--- Duc An Training/[02-09-2019] NKSGAME.cpp
+++ Duc An Training/[02-09-2019] NKSGAME.cpp
@@ -5,16 +5,28 @@ using namespace std;
 #define ll int64_t
 #define For(i, a, b) for(ll i = a; i <= b; i++)
 
-ll n, l1 = -1, l2 = -1, arr1[100010], arr2[100010];
+ll n, l1 = -1, l2 = -1, ans = 0, arr1[100010], arr2[100010], pros[100010];
 
-ll bSearch(ll x)
+ll bSearch_non_negative(ll x)
 {
-	ll l = 1, r = n;
-	while (r >= l)
+	ll l = l2, r = n;
+	while (r > l)
 	{
 		ll mid = (l + r) / 2;
-		if (abs(x + db[mid]))
+		if (abs(arr2[mid]) >= x) r = mid; else l = mid;
 	}
+	return r;
+}
+
+ll bSearch_negative(ll x)
+{
+	ll l = 1, r = l2;
+	while (r > l)
+	{
+		ll mid = (l + r) / 2;
+		if (abs(arr2[mid]) >= x) l = mid; else r = mid;
+	}
+	return r;
 }
 
 void process()
@@ -26,10 +38,17 @@ void process()
 		if (l1 == -1 && arr1[i] >= 0) l1 = i;
 		if (l2 == -1 && arr2[i] >= 0) l2 = i;
 	}
+	if (l1 == -1) l1 = n;
+	if (l2 == -1) l2 = n;
 	For(i, 1, n)
-	{
-
-	}
+	if (arr1[i] < 0)
+		pros[i] = bSearch_non_negative(arr1[i]);
+	else
+		pros[i] = bSearch_negative(arr1[i]);
+	ans = abs(arr1[1] + arr2[pros[1]]);
+	For(i, 2, n)
+	ans = min(ans, abs(arr1[i] + arr2[pros[i]]));
+	cout << ans << endl;
 }
 
 void input()
