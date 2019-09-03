ab119c4651ffb49eeabfed99c27cc83f10770edb
btdat2506 <btdat2506@users.noreply.github.com>
2019-09-03 08:04:32 +0700

    Update [02-09-2019] NKSGAME.cpp

--- Duc An Training/[02-09-2019] NKSGAME.cpp
+++ Duc An Training/[02-09-2019] NKSGAME.cpp
@@ -5,16 +5,18 @@ using namespace std;
 #define ll int64_t
 #define For(i, a, b) for(ll i = a; i <= b; i++)
 
-ll n, l1 = -1, l2 = -1, ans = 0, arr1[100010], arr2[100010], pros[100010];
+ll n, l1 = -1, l2 = -1, ans = 0, arr1[100010], arr2[100010], pos[100010];
 
 void process()
 {
 	sort(arr2+1, arr2+n+1);
 	For(i, 1, n)
-		pros[i] = lower_bound(arr2+1, arr2+n+1, -arr1[i]) - arr2;
-	ans = abs(arr1[1] + arr2[pros[1]]);
+		pos[i] = min(abs(arr1[i] + arr2[lower_bound(arr2+1, arr2+n+1, -arr1[i]) - arr2]),
+					abs(arr1[i] + arr2[upper_bound(arr2+1, arr2+n+1, -arr1[i]) - arr2]));
+
+	ans = pos[1];
 	For(i, 2, n)
-	ans = min(ans, abs(arr1[i] + arr2[pros[i]]));
+	ans = min(ans, pos[i]);
 	cout << ans << endl;
 }
 
