644c1d50dbc4a52cd0542e01ac8522d20ed7ed43
btdat2506 <btdat2506@users.noreply.github.com>
2019-09-05 23:01:38 +0700

    Created Educational Codeforces Round 72

---
+++ Educational Codeforces Round 72/A. Creating a Character.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll int64_t
+#define For(i, a, b) for(ll i = a; i <= b; i++)
+
+ll process(ll str, ll smart, ll exp)
+{
+	str -= smart;
+	smart = 0;
+	ll ans = exp - (str - 1);
+	ll str_origin = str, smart_origin = smart, exp_origin = exp;
+	while (str != smart + 1)
+	{
+
+	}
+}
+
+int main()
+{
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	ll t;
+	while(t--)
+	{
+		ll str, smart, exp;
+		cin >> str >> smart >> exp;
+		if (exp == 0)
+		{
+			if (str > smart) cout << 1 << endl; else cout << 0 << endl;
+		}
+		else
+		{
+			if (str < smart)
+			{
+				if (str + exp <= smart) cout << 0 << endl;
+				else
+				{
+					exp -= (smart - str - 1);
+					str += (smart - str - 1);
+					process(str, smart, exp);
+				}
+			}
+			else
+			if (str > smart)
+			cout << process(str, smart, exp) << endl;
+		}
+	}
+}
